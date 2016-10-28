7a28424890c193ab4adc1770e8a6eb8b6d7c4c83
paxapy <paxapy@gmail.com>
2016-10-28 17:09:34 +0300

    use proxy - fogoten line

--- bot.py
+++ bot.py
@@ -14,6 +14,8 @@ proxy_url = "http://proxy.server:3128"
 telepot.api._pools = {
     'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
 }
+telepot.api._onetime_pool_spec = (urllib3.ProxyManager,
+                                  dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
 
 """
 patriot bot
