b7558368fdf61fe40ce4a382f9d0363782cb41ec
paxapy <paxapy@gmail.com>
2016-10-31 10:17:26 +0300

    using some external data, plan is to refactor it to use db

--- bot.py
+++ bot.py
@@ -26,28 +26,22 @@ if config.USE_PROXY:
 
 
 class Patriot(telepot.aio.helper.ChatHandler):
-    music = [
-        'https://www.youtube.com/watch?v=vtw5u8Mhc_s',
-        'https://www.youtube.com/watch?v=Eu2Yq-qUw5Y',
-        'https://www.youtube.com/watch?v=L_utPjoRNsI',
-        'https://www.youtube.com/watch?v=b7dgQIkkmpQ',
-        'https://www.youtube.com/watch?v=37l7P5V1eXU',
-        'https://www.youtube.com/watch?v=F023drCD-M4'
-    ]
+    def choice_content(self):
+        types = ['videos', 'images']
+        with open('{}.txt'.format(random.choice(types)), 'r') as f:
+            link = random.choice(f.readlines())
+        return link
 
     async def open(self, initial_msg, seed):
         await self.sender.sendMessage('Пришло время подумать о родине, дружок')
         return True
 
     async def on_chat_message(self, msg):
-        content_type, chat_type, chat_id = telepot.glance(msg)
 
-        if content_type != 'text':
-            await self.sender.sendMessage('Скажи что-нибудь словами')
-            return
-
-        await self.sender.sendMessage('похоже, ты не достаточно патриот. вот немного лекарства')
-        await self.sender.sendMessage(random.choice(self.music))
+        await self.sender.sendMessage('похоже, ты не достаточно патриот. вот немного лекарства: \n {}'.format(
+            self.choice_content()
+        ))
+        await self.sender.send
 
     async def on__idle(self, event):
         await self.sender.sendMessage('судя по всему ты не хочешь скрепляться, вызываю гибдд')
@@ -56,7 +50,7 @@ class Patriot(telepot.aio.helper.ChatHandler):
 
 bot = telepot.aio.DelegatorBot(config.TOKEN, [
     pave_event_space()(
-        per_chat_id(), create_open, Patriot, timeout=10),
+        per_chat_id(), create_open, Patriot, timeout=100),
 ])
 
 loop = asyncio.get_event_loop()
